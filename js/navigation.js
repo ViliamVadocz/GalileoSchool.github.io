cabca6129fa42e620edd125f0d2b430afb51fd1b
MidasKing67 <cosxmidas67@gmail.com>
2019-09-30 18:26:23 +0200

    Added Documentation for the JavaScript (Educational Purposes)
    In case of any questions or a weak documentation feel free to contact me

--- js/navigation.js
+++ js/navigation.js
@@ -1,16 +1,45 @@
-console.log("Javascript Loaded!");
-const menuBtn = document.getElementById("nav-btn");
-const NavMenuDesk = document.getElementById('nav-desk');
+/** This is a debug message that tells me wether or not my javascript was loaded, this debug message can be checked in developer tools in chrome under the tab named console
+ * Difficulty: Easy
+*/
+console.log("Javascript Loaded!"); 
 
-menuBtn.addEventListener("click", toggle_menu, false);
+/** Here I'm trying to select a specific element (in this case it is the navigation button) from my html file using its id(identifier) which is unique through-out the document 
+ * Difficulty: Easy
+*/
+const menuBtn = document.getElementById("nav-btn"); 
+
+/** The same as above only now I'm trying to select a different element (in this case it's the menu itself) 
+ * Difficulty: Easy
+*/
+const NavMenuDesk = document.getElementById('nav-desk'); 
+
+/** Adding an Event Listener for an action (in my case it's the click on a button) done on my object, and passing a function that should be carried out when the event occurs.
+ * Difficulty: Medium
+ */
+menuBtn.addEventListener("click", toggle_menu, false); 
+
+/** The same as above but this time I'm actually listening when a browser is being resized. So whenever you extend or shrink your Internet Browser this event will fire up, again completing a function that I've passed into the event.
+ * Difficulty: Medium
+ */
 window.addEventListener("resize", check_resize, false);
 
+/** Function that shows and hides our menu on mobile devices
+ * Difficulty: Harder
+ * @description What this function does to achieve the effect of hiding and showing our menu after clicking a button on a mobile device, 
+ * is that it toggles (either adds or removes depends on wether or not the object already has the class) class that shows or hides our element.
+ * Yeah, blur effect is also added using JavaScript and it's powerful events. 
+ */
 function toggle_menu(){
     NavMenuDesk.classList.toggle('show');
     menuBtn.classList.toggle('active-nav-btn');
-    document.querySelector('.content').classList.toggle('blur');
+    document.querySelector('.content').classList.toggle('blur');/** Here I add blur effect to background when menu is visible, thus making human eye more focused on the menu rather than on the content in the background */
 }
 
+/** Function that checks the browser size whenever it is resized and evaluates the following statements
+ * Difficulty: Harder
+ * @description This function is crucial to get rid of annoying bugs that can occur when transitioning from mobile viewport to desktop viewport. 
+ * Such as when you leave your menu open on a mobile viewport and you extend your browser to desktop viewport the blur effect will still be active, and that's a big NO NO!
+ */
 function check_resize(){
     if(window.innerWidth >= 740){
         if(document.querySelector('.content').classList.contains('blur'))
