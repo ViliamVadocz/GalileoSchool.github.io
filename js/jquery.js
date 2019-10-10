83668542f06a0e374d0be2adc5370f685da2ff52
Adrian Jakubcik <cosxmidas67@gmail.com>
2019-10-10 23:47:36 +0200

    Added Search Bar
    Language selector should be fully functional
    
    Some Javascript and CSS Tweaks

--- js/jquery.js
+++ js/jquery.js
@@ -1,33 +1,69 @@
 // This tells the browser to execute certain function as soon as it is ready (as soon as the page is fully loaded)
-$(document).ready(function () {
-  
-  // Using Id I'm selecting the element with the given Id in this case it is the navigation button
-  var navBtn = '#nav-btn';
+$(document).ready(function() {
 
-  // "Installing" Click event listener on the selected element (When clicked the navigation box slides up and down)
-  $(navBtn).click(function () { 
-    $('.navbar').slideToggle('fast'); 
-    $('.language-dropdown-content').fadeOut(200);
-  }); 
+    // Using Id I'm selecting the element with the given Id in this case it is the navigation button
+    var navBtn = '#nav-btn';
 
-  // Using Id I'm selecting the element with the given Id in this case it is the language button
-  var langBtn = '#langBtn';
-
-  // "Installing" Click event listener on the selected element (When clicked the language box slides up and down)
-  $(langBtn).click(
-    function(){
-      $('.language-dropdown-content').fadeToggle(200);
-      return;
+    // "Installing" Click event listener on the selected element (When clicked the navigation box slides up and down)
+    $(navBtn).click(function() {
+        $('.navbar').slideToggle('fast');
+        $('.dropdown-content').fadeOut(200);
     });
-  
-  $(document).click(function(){
-    if(!$('.language-dropdown-content').is(":hover") && !$('.language-dropdown-content').is(":hidden") && !$(langBtn).is(":hover"))
-    {
-      $('.language-dropdown-content').fadeOut(200);
-    }
-  });
 
-  //Use the code below for the desktop version (It is the hover version)
+    // Using Id I'm selecting the element with the given Id in this case it is the language button
+    var langBtn = '#langBtn';
+
+    // "Installing" Click event listener on the selected element (When clicked the language box slides up and down)
+    $(langBtn).click(
+        function() {
+            $('.language-dropdown-content').fadeToggle(200);
+            return;
+        });
+
+    var searchBtn = '#search';
+    var searchInput = '.search-input';
+    var searching = false;
+
+    $(searchBtn).click(
+        function() {
+            if (!searching) {
+                slideIn();
+                searching = true;
+            } else {
+                slideOut();
+                searching = false;
+            }
+            return;
+        });
+
+    $(document).click(function() {
+        if (!$('.language-dropdown-content').is(":hover") && !$('.language-dropdown-content').is(":hidden") && !$(langBtn).is(":hover")) {
+            $('.language-dropdown-content').fadeOut(200);
+        } else if (!$(searchInput).is(":hover") && !$(searchInput).is(":hidden") && !$(searchBtn).is(":hover")) {
+            /*if(!$(".search-input").val() == ''){
+              $('.search-input').val('');
+            }*/
+            slideOut();
+            searching = false;
+        }
+    });
+
+
+    function slideIn(selector = searchInput) {
+        $(selector).animate({
+            "width": "40vw",
+            "opacity": .9
+        }, 500);
+    };
+
+    function slideOut(selector = searchInput) {
+        $(selector).animate({
+            "width": "0vw",
+            "opacity": 0,
+        }, 500);
+    };
+
+    //Use the code below for the desktop version (It is the hover version)
 
     /*var langBtn = '#langBtn';
   $(langBtn).hover(function () {
--- js/navigation.js
+++ js/navigation.js
@@ -1,31 +1,36 @@
 /** This is a debug message that tells me wether or not my javascript was loaded, this debug message can be checked in developer tools in chrome under the tab named console
  * 
  * Difficulty: Easy
-*/
-console.log("Javascript Loaded!"); 
+ */
+console.log("Javascript Loaded!");
 
 /** Here I'm trying to select a specific element (in this case it is the navigation button) from my html file using its id(identifier) which is unique through-out the document 
  * 
  * Difficulty: Easy
-*/
+ */
 const menuBtn = document.getElementById("nav-btn");
 
 const menuBtnImg = document.getElementById('menu-btn-img');
 var original_attribute_src = menuBtnImg.getAttribute('src');
-var exit_img_src = "images/icons/close.png"; 
+var url = window.location.pathname;
+var parsed_dir = url.substring(url.lastIndexOf("/") - 2, url.lastIndexOf("/"));
+if (parsed_dir == "sk")
+    var exit_img_src = "../images/icons/close.png";
+else
+    var exit_img_src = "images/icons/close.png";
 
 /** The same as above only now I'm trying to select a different element (in this case it's the menu itself) 
  * 
  * Difficulty: Easy
-*/
-const NavMenuDesk = document.getElementById('nav-desk'); 
-var NavMenu = document.getElementById('Navigation');
+ */
+const NavMenuDesk = document.getElementById('nav-desk');
+const NavMenu = document.getElementById('Navigation');
 
 /** Adding an Event Listener for an action (in my case it's the click on a button) done on my object, and passing a function that should be carried out when the event occurs.
  * 
  * Difficulty: Medium
  */
-menuBtn.addEventListener("click", toggle_menu, false); 
+menuBtn.addEventListener("click", toggle_menu, false);
 
 /** The same as above but this time I'm actually listening when a browser is being resized. So whenever you extend or shrink your Internet Browser this event will fire up, again completing a function that I've passed into the event.
  * 
@@ -40,12 +45,12 @@ window.addEventListener("resize", check_resize, false);
  * is that it toggles (either adds or removes depends on wether or not the object already has the class) class that shows or hides our element.
  * Yeah, blur effect is also added using JavaScript and it's powerful events. 
  */
-function toggle_menu(){
-    if(menuBtnImg.getAttribute('src') == original_attribute_src)
+function toggle_menu() {
+    if (menuBtnImg.getAttribute('src') == original_attribute_src)
         menuBtnImg.setAttribute('src', exit_img_src);
     else
         menuBtnImg.setAttribute('src', original_attribute_src);
-    document.querySelector('.content').classList.toggle('blur');/** Here I add blur effect to background when menu is visible, thus making human eye more focused on the menu rather than on the content in the background */
+    document.querySelector('.content').classList.toggle('blur'); /** Here I add blur effect to background when menu is visible, thus making human eye more focused on the menu rather than on the content in the background */
 }
 
 /** Function that checks the browser size whenever it is resized and evaluates the following statements
@@ -54,18 +59,19 @@ function toggle_menu(){
  * @description This function is crucial to get rid of annoying bugs that can occur when transitioning from mobile viewport to desktop viewport. 
  * Such as when you leave your menu open on a mobile viewport and you extend your browser to desktop viewport the blur effect will still be active, and that's a big NO NO!
  */
-function check_resize(){
-    if(window.innerWidth >= 670){
-        if(document.querySelector('.content').classList.contains('blur'))
+function check_resize() {
+    if (window.innerWidth >= 670) {
+        if (document.querySelector('.content').classList.contains('blur')) {
             document.querySelector('.content').classList.remove('blur');
-        if (window.getComputedStyle(NavMenu).display == "none") {
-            NavMenu.style.display = "flex";
         }
-        else
+        if (menuBtnImg.getAttribute('src') == exit_img_src) {
+            menuBtnImg.setAttribute('src', original_attribute_src);
+        }
+        if (window.getComputedStyle(NavMenu).display == "none" || window.getComputedStyle(NavMenu).display == "block") {
+            NavMenu.style.display = "flex";
+        } else
             return;
-    }
-    else if(window.innerWidth < 670)
-    {
+    } else if (window.innerWidth < 670) {
         if (window.getComputedStyle(NavMenu).display == "flex") {
             NavMenu.style.display = "none";
         }
