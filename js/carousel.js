5ef2bbf49de5479e95baecd58fc92d9820baeb40
Viliam Vadocz <viliam.vadocz@gmail.com>
2019-11-11 20:15:57 +0100

    Small tweaks

--- js/carousel.js
+++ js/carousel.js
@@ -15,8 +15,6 @@ function show_slide(wanted_slide) {
     var slides = document.getElementsByClassName("slide");
     var dots = document.getElementsByClassName("dot");
 
-    // var prev_slide = slide_index
-
     // Set slide_index. Loop back around if needed.
     if (wanted_slide > slides.length) {
         slide_index = 1
