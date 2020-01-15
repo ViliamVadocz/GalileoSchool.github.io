0f2a23d829671ad83523ff795aec2f57f27dbf17
Viliam Vadocz <viliam.vadocz@gmail.com>
2020-01-15 21:09:42 +0100

    Extra documentation and signatures are constants

--- build.js
+++ build.js
@@ -1,12 +1,28 @@
 // this is the build script
 // it takes files from source/ and transpiles them into build/
 // it takes components from components/ and substitutes them into all .html
-//  files in source that use the handlebar syntax of {{ ... }}
+//  files that use the handlebar syntax of {{ ... }}
+// components can also use this syntax {{ ... }} to place other components inside
+//  referred to as 'nested' components
 // all non-html files are simply copied
+
+// this program actually transpiles twice, creating two copies of html files, 
+//  once using english components and a second time using slovak components
+// slovak components are those placed in the components/sk/ folder and 
+//  components/en/ for english
+// both transpilations can use the shared components inside components/
+
+// components can be 'compound' if they use the appropriate syntax (defined below)
+// this acts as if you had multiple components, but put into one file
+//  and accessed using {{component.subcomponent}}
 
 const fs = require('fs') // for manipulation of files
 const glob = require('glob') // for finding the right files
 const Handlebars = require('handlebars') // for using handlebars
+
+// syntax for compound components
+const OPENING_SIGNATURE = 'TEMPORARY_OPENING_SIGNATURE ' // notice the space on end
+const CLOSING_SIGNATURE = ' TEMPORARY_CLOSING_SIGNATURE' // notice the space in front
 
 // getter function for directory name
 function getDirname () {
@@ -51,6 +67,7 @@ function makeComponentDictionary(globPattern) {
 
 	const componentSourceFiles = glob.sync(globPattern)
 	const components = {}
+	// loop over found components matching the glob pattern
 	for (const componentSourceFile of componentSourceFiles) {
 		const componentName = componentSourceFile
 			.split('/').pop() // get the actual filename
@@ -58,29 +75,33 @@ function makeComponentDictionary(globPattern) {
 	
 		console.log('Found component', componentName)
 
-		// parse component
+		// parse component (separate into subcomponents if it is compound)
 		const fileString = fs.readFileSync(componentSourceFile, "utf8")
-		if (fileString.indexOf('TEMPORARY_OPENING_SIGNATURE ') == 0) {
+		if (fileString.indexOf(OPENING_SIGNATURE) == 0) {
 			console.log(componentName, 'was identified as a compound component')
 
-			// split by sections
-			let sections = fileString.split('\r\nTEMPORARY_OPENING_SIGNATURE ')
-			sections[0] = sections[0].replace('TEMPORARY_OPENING_SIGNATURE ', '')
-			// seperate sections into name and content
-			sections = sections.map(section => section.split(' TEMPORARY_CLOSING_SIGNATURE\r\n'))
+			// split by sections / subcomponents
+			let sections = fileString.split('\r\n' + OPENING_SIGNATURE)
+			sections[0] = sections[0].replace(OPENING_SIGNATURE, '')
+			// separate sections into name and content
+			sections = sections.map(section => section.split(CLOSING_SIGNATURE + '\r\n'))
 			// add component sections to dictionary
 			let componentDict = {}
-			sections.map(section => componentDict[section[0]] = new Handlebars.SafeString(section[1]))
+			sections.forEach(
+				section => 
+				componentDict[section[0]] = new Handlebars.SafeString(section[1])
+				)
 			// add compound component to main component dictionary
 			components[componentName] = componentDict
 		}
 		else {
+			// otherwise if not compound, just add them normally
+
 			// Handlebars escapes the '<' and '>' characters by default, so we need to
 			// explain that the strings are safe
 			// API reference: http://handlebarsjs.com/reference.html
 			components[componentName] = new Handlebars.SafeString(fileString)
 		}
-	
 		
 	}
 	return components
@@ -101,19 +122,20 @@ function transpileUsingNestedHandlebars(fileString, fileToCreate, components) {
 	console.log('Transpiled file', fileToCreate)
 }
 
+// first we prepare all the folders in the build folder
 // create the target build/ folder if it doesn't exist yet
 makeFolder('./build/')
 // create base html folders (en/sk versions)
 makeFolder(getDirname() + '/build/html/')
 makeFolder(getDirname() + '/build/html/en/')
 makeFolder(getDirname() + '/build/html/sk/')
-
-// first we prepare all folders in the build folder
+// the remaining folders are not hardcoded,
+//  they are added as they are found in source/
 const sourceFolders = glob.sync(getDirname() + '/source/**/', {})
 for (const sourceFolder of sourceFolders) {
 	// identifies the new folder to be created
 
-	// checks if it is in the html folder
+	// checks if the source folder is in the html folder
 	if (sourceFolder.includes('/html/')) {
 		// create en and sk copies of the folder
 		const enFolderToCreate = sourceFolder.replace(
